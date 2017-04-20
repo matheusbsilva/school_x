1dd95caa864e58def00958c2074a6b2c32510fce
matheusbsilva <matheusbattista@hotmail.com>
2017-04-20 12:35:01 +0000

    Creates alumn and parent controller.

--- app/controllers/alumns_controller.rb
+++ app/controllers/alumns_controller.rb
@@ -7,6 +7,18 @@ class AlumnsController < ApplicationController
     @alumn = Alumn.new
   end
 
+  def show
+    @alumn = Alumn.find(params[:id])
+  end
+
+  def create
+    @alumn = Alumn.new(alumn_params)
+    if (@alumn.save)
+      redirect_to @alumn
+    else
+      render 'new'
+  end
+
   private
   def alumn_params
     params.requite(:alumn).permit(:name,:address,:registry,:password,:parent_id)
